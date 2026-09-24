8a0039cfd01fca3402ab5824483d58cf8ebef5ca
Chan-yeong Kang <rrrfffrrr@naver.com>
2026-09-24 17:14:30 +0900

    docs: fetch the CLI tarball by tag name
    codeload serves /tar.gz/<tag> for a tag; the refs/tags/ form answered 404 right after the tag was pushed.

--- examples/shapeup.sh
+++ examples/shapeup.sh
@@ -16,4 +16,4 @@ export GH_TOKEN SHAPEUP_REPOSITORY
 MSYS_NO_PATHCONV=1 exec docker run --rm -i -e GH_TOKEN -e SHAPEUP_REPOSITORY \
   -v "$host:/workspace:ro" -w /workspace -v github-shapeup-npm:/root/.npm \
   node:24-bookworm-slim \
-  npx --yes "https://codeload.github.com/vulpes33/github-shapeup/tar.gz/refs/tags/$version" "$@"
+  npx --yes "https://codeload.github.com/vulpes33/github-shapeup/tar.gz/$version" "$@"
